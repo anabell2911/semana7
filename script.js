b781b7cf83e40a8eacbba06fa1a93b352aa6199a
anabell2911 <71998176+anabell2911@users.noreply.github.com>
2021-03-10 07:21:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 var array=[];
-var i=0;
+var x=0;
 var	mayor;
-var a=document.getElementById('entrada');
-function fingresa(){
-	if(i<5)
-	{
-		if (i==0){
-			mayor=parseInt(a.value);
-		}
-		else if(a.value>mayor)
-		{
-			mayor=parseInt(a.value);
-		}
+var a=document.getElementById('enter');
+function ingresa(){
+	if(x<5)
+	{if (x==0){mayor=parseInt(a.value);}
+	else if(a.value>mayor)
+	{mayor=parseInt(a.value);}
 		console.log(mayor);
-		i+=1;
-		array[i]=a.value;
-	}
-		
-}
+		x+=1;
+		array[x]=a.value;}}
 
-function fmostrar()
-{
-	document.getElementById('resultado').value=mayor;
-}
+function mostrar()
+{document.getElementById('resultado').value=mayor;}
